7caebbbf15a51f3d8e63993491839778617aefaf
TirumaliSaiTeja <tsaiteja7@gmail.com>
2021-07-29 23:47:02 +0530

    Day 1- Class syntax

---
+++ DS_Introduction/Intro.js
@@ -0,0 +1,17 @@
+//ES2015 Class
+
+// Class- A blueprint for creating objects with pre-defined properties and methods
+// Constructor- method to create new objects
+// Instanciate values to the class using new keyword
+
+// Syntax of class:
+
+class Student{
+    constructor(firstName, lastName){
+        this.firstName= firstName;
+        this.lastName=lastName;
+    }
+}
+
+let firstName= new.Student("sai", "teja");
+let SecondName= new.Student("nani", "nani");
